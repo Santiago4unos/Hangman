107a990335a556e850113ef2e1077909f4e56072
Santiago Muñoz <santimunozruiz@gmail.com>
2022-08-08 11:39:15 -0500

    Debugging finished

--- debugging.py
+++ debugging.py
@@ -1,11 +1,21 @@
+from multiprocessing.sharedctypes import Value
+
+
 def divisors(num):
     divisors = [i for i in range(1, num+1) if num % i == 0]
     return divisors
 
 
 def run():
-    num = int(input("Escribe un número: "))
-    print(divisors(num))
+    error = True
+    while error == True:
+        try:
+            num = int(input("Escribe un número: "))
+            print(divisors(num))
+            error = False
+        except ValueError:
+            print("No se pueden ingresar strings")
+            error = True
 
 if __name__ == "__main__":
     run()
