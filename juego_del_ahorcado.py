350d8484f1018c7e06c740c4e892aaf2107082c2
Santiago Muñoz <santimunozruiz@gmail.com>
2022-08-08 22:04:12 -0500

    Faltan algunos errores que arreglar

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -1,5 +1,37 @@
+import random
+
+def chooseword():
+    size_of_list = len(word_list)
+    chosen_word = random.randint(1, size_of_list)
+    word_list[chosen_word]
+        
+
+
+def read():
+    word_list = []
+    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+        for word in f:
+            word_list.append(word)
+    return word_list
+
+
 def run():
-    pass
+    read()
+    win = False
+    print("Bienvenido al juego")
+    print("""
+ __ __   ____  ____    ____  ___ ___   ____  ____  
+|  |  | /    ||    \  /    ||   |   | /    ||    \ 
+|  |  ||  o  ||  _  ||   __|| _   _ ||  o  ||  _  |
+|  _  ||     ||  |  ||  |  ||  \_/  ||     ||  |  |
+|  |  ||  _  ||  |  ||  |_ ||   |   ||  _  ||  |  |
+|  |  ||  |  ||  |  ||     ||   |   ||  |  ||  |  |
+|__|__||__|__||__|__||___,_||___|___||__|__||__|__|
+                                                   
+        """)
+    while win == False:
+        chooseword()
+        print()
 
 
 if __name__ == "__main__":
