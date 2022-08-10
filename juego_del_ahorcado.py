113ef39024898968dad4cee71ee12d58e910303e
Santiago Muñoz <santimunozruiz@gmail.com>
2022-08-09 23:51:14 -0500

    Completé el juego, aunque tuve que sacrificar la mecánica de las vidas porque ya estoy harto

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -1,22 +1,67 @@
+from functools import reduce
 import random
+import os
 
-def chooseword():
-    size_of_list = len(word_list)
-    chosen_word = random.randint(1, size_of_list)
-    word_list[chosen_word]
-        
+def normalize(chosen_word):
+    vocals = ("a", "e", "i", "o", "u")
+    vocals_with_accent_marks = ("á", "é", "í", "ó", "ú")
+    for y in range(5):
+        chosen_word = chosen_word.replace(vocals_with_accent_marks[y], vocals[y])
+    return chosen_word
+
+def game(chosen_word, letters, hidden_word, size_of_letters, correct_letters):
+    retry = True
+    hidden_word = reduce(lambda a, b: a + b, correct_letters)
+    if hidden_word == chosen_word:
+        return True
+    for y in range(len(letters)):
+        if correct_letters[y] == "-":
+            continue
+
+    if hidden_word == chosen_word:
+        return True      
+    while retry == True:
+        os.system("cls")
+        print("Adivina la palabra")
+        print("¿Podrás hacerlo?")
+        print(hidden_word)
+        guessed_letter = input("Escribe una letra: ")
+        if len(guessed_letter) < 1:
+            input("Necesitas ingresar una letra, presiona enter para intentar de nuevo")
+
+        elif len(guessed_letter) > 1:
+            input("No puedes ingresar más de una letra, presiona enter para intentar de nuevo")
+
+        try:
+            int(guessed_letter)
+            input("No puedes ingresar números, presiona enter para intentar de nuevo")
+
+        except ValueError:
+            retry = False
+    if hidden_word == chosen_word:
+        return True
+    for y in range(size_of_letters):
+        for y in range(size_of_letters):
+            if guessed_letter == letters[y]:
+                where_it_is = letters.index(guessed_letter)
+                correct_letters.pop(where_it_is)
+                correct_letters.insert(where_it_is, guessed_letter)
+    return False
 
 
 def read():
     word_list = []
     with open("./archivos/data.txt", "r", encoding="utf-8") as f:
-        for word in f:
-            word_list.append(word)
-    return word_list
+        word_list = [word for word in f]
+    size_of_list = len(word_list)
+    chosen_num = random.randint(1, size_of_list)
+    chosen_word = word_list[chosen_num]
+    chosen_word = chosen_word.replace("\n", "")
+    return chosen_word
 
 
 def run():
-    read()
+    chosen_word = read()
     win = False
     print("Bienvenido al juego")
     print("""
@@ -29,9 +74,17 @@ def run():
 |__|__||__|__||__|__||___,_||___|___||__|__||__|__|
                                                    
         """)
+    input("Presiona enter para continuar")
+    letters = [g for g in normalize(chosen_word)]
+    for i in range(len(chosen_word)):
+        hidden_word = chosen_word.replace(chosen_word[i], "-")
+    correct_letters = []
+    for i in range(len(letters)):
+        correct_letters.append("-")
+    size_of_letters = len(letters)
     while win == False:
-        chooseword()
-        print()
+        win = game(chosen_word, letters, hidden_word, size_of_letters, correct_letters)
+    print("¡Felicidades, has ganado!, la palabra era: " + chosen_word)
 
 
 if __name__ == "__main__":
